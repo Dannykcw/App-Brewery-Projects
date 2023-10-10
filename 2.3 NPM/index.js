f5a962071c2980fb71e1b66e9fb7b706dde83b5c
Dannykcw <106027442+Dannykcw@users.noreply.github.com>
2023-10-09 20:54:01 -0700

    added section 2.3 NPM solution

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,7 @@
+import generateName from "sillyname";
+import superheros from "superheroes";
+let sillyname = generateName();
+let superhero = superheros.random();
+console.log(`My name is ${sillyname}`);
+console.log(`I am ${superhero}!`);
+
