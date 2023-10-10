874e05b2aa3ebbc42802a180ae33afa72eee34ed
Dannykcw <106027442+Dannykcw@users.noreply.github.com>
2023-10-09 17:09:54 -0700

    initial commit

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("button")
+
+btn.addEventListener("click", () => {
+    /*
+    document.lastElementChild.lastElementChild.children[3].lastElementChild.innerHTML = "I win!"
+    */
+    document.querySelector('ul').lastElementChild.innerText = "Angela"
+});
