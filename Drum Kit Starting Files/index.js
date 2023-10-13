be2d4a8d893826c8fa8bad16d8325d61ad6b754f
Dannykcw <106027442+Dannykcw@users.noreply.github.com>
2023-10-12 19:02:33 -0700

    Added event listeners to buttons under the div

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,1 +1,22 @@
-alert("Somestuff");
+//The below means that even if you click outside of the buttons (since it is outside of the divs), the eventlistener is still triggered.
+// document.querySelector(".set").addEventListener("click", () => {
+//     alert("I was clicked");
+// });
+
+//This uses a for each loop
+// const selection = document.querySelectorAll(".drum");
+
+// selection.forEach(btu => {
+//     btu.addEventListener("click", handleClick);
+// });
+
+// function handleClick() {
+//     alert("I was clicked!");
+// }
+
+//This uses a conditional and targets butttons inside a certain div only. See https://javascript.info/bubbling-and-capturing
+const wrapper = document.querySelector(".set");
+
+wrapper.addEventListener("click", (event) => {
+    console.dir(event.target.nodeName);
+});
