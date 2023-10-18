a8c863ac7847da541a54ff85c3fbcbcbd3b4d31d
Dannykcw <106027442+Dannykcw@users.noreply.github.com>
2023-10-18 10:48:58 -0700

    Added case and sound for each button

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,9 +1,9 @@
-//The below means that even if you click outside of the buttons (since it is outside of the divs), the eventlistener is still triggered.
+// The below means that even if you click outside of the buttons (since it is outside of the divs), the eventlistener is still triggered.
 // document.querySelector(".set").addEventListener("click", () => {
 //     alert("I was clicked");
 // });
 
-//This uses a for each loop
+// This uses a for each loop
 // const selection = document.querySelectorAll(".drum");
 
 // selection.forEach(btu => {
@@ -13,10 +13,47 @@
 // function handleClick() {
 //     alert("I was clicked!");
 // }
+// This uses a conditional and targets butttons inside a certain div only. See https://javascript.info/bubbling-and-capturing
 
-//This uses a conditional and targets butttons inside a certain div only. See https://javascript.info/bubbling-and-capturing
-const wrapper = document.querySelector(".set");
+const wrapper = document.querySelector('.set')
 
-wrapper.addEventListener("click", (event) => {
-    console.dir(event.target.nodeName);
-});
+wrapper.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON'
+    if (isButton) {
+        const btnInnerHTML = event.target.innerHTML
+        let audio
+        console.dir(btnInnerHTML)
+        switch (btnInnerHTML) {
+            case 'w':
+                audio = new Audio('./sounds/tom-1.mp3')
+                audio.play()
+                break
+            case 'a':
+                audio = new Audio('./sounds/tom-2.mp3')
+                audio.play()
+                break
+            case 's':
+                audio = new Audio('./sounds/tom-3.mp3')
+                audio.play()
+                break
+            case 'd':
+                audio = new Audio('./sounds/tom-4.mp3')
+                audio.play()
+                break
+            case 'j':
+                audio = new Audio('./sounds/snare.mp3')
+                audio.play()
+                break
+            case 'k':
+                audio = new Audio('./sounds/crash.mp3')
+                audio.play()
+                break
+            case 'l':
+                audio = new Audio('./sounds/kick-bass.mp3')
+                audio.play()
+                break
+            default:
+                break
+        }
+    }
+})
